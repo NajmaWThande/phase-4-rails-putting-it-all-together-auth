bc8320f921d3c97c2e1bd5fde5143f3b592408f7
Najma <najmathande@gmail.com>
2023-07-02 16:55:56 +0300

    done

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+    def create
+      user = User.new(user_params)
+      if user.save
+        session[:user_id] = user.id
+        render json: user, status: :created
+      else
+        render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+
+    def show
+        if logged_in?
+          user = User.find(session[:user_id])
+          render json: user, status: :ok
+        else
+          render json: { error: 'Unauthorized' }, status: :unauthorized
+        end
+      end
+  
+    private
+  
+    def user_params
+      params.require(:user).permit(:username, :password, :password_confirmation, :image_url, :bio)
+    end
+  end
+  
